ea54603b054ca35061e89cb495cbd1566f8e0e38
Sowmya1114 <sabbavarapussri@gmail.com>
2026-07-18 13:43:23 +0530

    Celsius to Fahrenheit and vice versa converter

---
+++ Day-1/CelciusToFarenheit&FarenheitToCelcius.py
@@ -0,0 +1,6 @@
+Cel=int(input("Enter a celcius degree: "))
+cel_res=(Cel*(9/5))+32
+print(f"{int(cel_res)}F")
+Far=int(input("Enter a farhenheat degree: "))
+far_res=(Far-32)*(5/9)
+print(f"{int(far_res)}C")
