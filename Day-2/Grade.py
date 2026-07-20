9ab83c9516a424e5d7889b29f87c0090cd16019e
Sowmya1114 <sabbavarapussri@gmail.com>
2026-07-20 19:54:34 +0530

    grading system

---
+++ Day-2/Grade.py
@@ -0,0 +1,11 @@
+marks=int(input("Enter a number: "))
+if marks>=90 and marks<=100:
+    print("A")
+elif marks>79 and marks<90:
+    print("B")
+elif marks>69 and marks<80:
+    print("C")
+elif marks>59 and marks<70:
+    print("D")
+else:
+    print("F")
